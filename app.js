d3a1048cad05d96c744485a184ce07ab9e24686d
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2022-06-05 01:21:39 +0530

    add db connection

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
+
+// Connecting to DB
+const DB_URL = process.env.DB_URL
+  ? process.env.DB_URL
+  : "mongodb://localhost:27017/schedule-backend-nodejs";
+
+mongoose.connect(DB_URL).catch((error) => {
+  console.error(error);
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => console.log("Database connected!"));
 
 app.get("/", (req, res) => {
   res.send("Hello");
