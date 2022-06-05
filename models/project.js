1b059326fe4d3146f6e87ce612e147f668a3236d
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2022-06-05 11:35:18 +0530

    add project schema and model

---
+++ models/project.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ProjectSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  details: {
+    type: String,
+    required: true,
+  },
+  tasks: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Task",
+    },
+  ],
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
