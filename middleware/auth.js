8f50d0ec4a575aa5fcd773647f4af60eceb3af08
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2022-06-12 12:44:43 +0530

    fix auth middleware bug

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 exports.isLoggedIn = (req, res, next) => {
-  if (!req.isAuthenticated) {
+  if (!req.isAuthenticated()) {
     res.status(401).json({
       msg: "Login to access this route",
     });
